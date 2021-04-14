7fad80fab193d4a9dfdbc1c30b82a471123eb9a2
Thagus <agu.alcarin@gmail.com>
2021-04-13 19:49:33 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,103 @@
+const { execSync } = require("child_process");
+const { existsSync, readFileSync } = require("fs");
+const { join } = require("path");
+
+/**
+ * Logs to the console
+ */
+const log = (msg) => console.log(`\n${msg}`); // eslint-disable-line no-console
+
+/**
+ * Exits the current process with an error code and message
+ */
+const exit = (msg) => {
+	console.error(msg);
+	process.exit(1);
+};
+
+/**
+ * Executes the provided shell command and redirects stdout/stderr to the console
+ */
+const run = (cmd, cwd) => execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
+
+/**
+ * Determines the current operating system (one of ["mac", "windows", "linux"])
+ */
+const getPlatform = () => {
+	switch (process.platform) {
+		case "darwin":
+			return "mac";
+		case "win32":
+			return "windows";
+		default:
+			return "linux";
+	}
+};
+
+/**
+ * Returns the value for an environment variable (or `null` if it's not defined)
+ */
+const getEnv = (name) => process.env[name.toUpperCase()] || null;
+
+/**
+ * Sets the specified env variable if the value isn't empty
+ */
+const setEnv = (name, value) => {
+	if (value) {
+		process.env[name.toUpperCase()] = value.toString();
+	}
+};
+
+/**
+ * Returns the value for an input variable (or `null` if it's not defined). If the variable is
+ * required and doesn't have a value, abort the action
+ */
+const getInput = (name, required) => {
+	const value = getEnv(`INPUT_${name}`);
+	if (required && !value) {
+		exit(`"${name}" input variable is not defined`);
+	}
+	return value;
+};
+
+/**
+ * Installs NPM dependencies and builds/releases the Electron app
+ */
+const runAction = () => {
+	const platform = getPlatform();
+	const release = getInput("release", true) === "true";
+	const pkgRoot = getInput("package_root", true);
+	const args = getInput("args") || "";
+	const pkgJsonPath = join(pkgRoot, "package.json");
+
+	log(`Will run Yarn commands in directory "${pkgRoot}"`);
+
+	// Make sure `package.json` file exists
+	if (!existsSync(pkgJsonPath)) {
+		exit(`\`package.json\` file not found at path "${pkgJsonPath}"`);
+	}
+
+	// Copy "github_token" input variable to "GH_TOKEN" env variable (required by `electron-builder`)
+	setEnv("GH_TOKEN", getInput("github_token", true));
+
+	// Require code signing certificate and password if building for macOS. Export them to environment
+	// variables (required by `electron-builder`)
+	if (platform === "mac") {
+		setEnv("CSC_LINK", getInput("mac_certs"));
+		setEnv("CSC_KEY_PASSWORD", getInput("mac_certs_password"));
+	} else if (platform === "windows") {
+		setEnv("CSC_LINK", getInput("windows_certs"));
+		setEnv("CSC_KEY_PASSWORD", getInput("windows_certs_password"));
+	}
+
+	// Disable console advertisements during install phase
+	setEnv("ADBLOCK", true);
+
+	log(`Installing dependencies using Yarn…`);
+	run("yarn", pkgRoot);
+
+	log(`Building${release ? " and releasing" : ""} the Electron app… \n`);
+	run(`yarn run build --${platform} ${(release ? "--publish always" : "")} ${args}`, pkgRoot);
+};
+
+runAction();
